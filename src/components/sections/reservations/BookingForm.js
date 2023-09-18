78c55ef0b7317e838ab84bb2a0750c8fd03665e2
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-09-18 20:29:46 +0530

    final commit

--- src/components/sections/reservations/BookingForm.js
+++ src/components/sections/reservations/BookingForm.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react';
 import {fetchAPI} from '../../../BookingAPI';
 import '../../../styles/sections/BookingForm.css';
 
-
 function BookingForm({ dispatch,submitForm}) {
   const [date, setDate] = useState('');
   const [time, setTime] = useState('17:00');
@@ -15,6 +14,16 @@ function BookingForm({ dispatch,submitForm}) {
   const [phone, setPhone] = useState('');
   const [setSubmitted] = useState(false); // Track form submission
   const [availableTimes, setAvailableTimes] = useState([]); // Store available times
+
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    date: '',
+    time: '',
+    guests: '',
+  });
 
   useEffect(() => {
     // Fetch available times when the date changes
@@ -31,9 +40,131 @@ function BookingForm({ dispatch,submitForm}) {
     }
   }, [date]);
 
+  const validateFirstName = () => {
+    if (firstName.trim() === '') {
+      setErrors((prevState) => ({
+        ...prevState,
+        firstName: 'First Name is required',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        firstName: '',
+      }));
+    }
+  };
+
+  const validateLastName = () => {
+    if (lastName.trim() === '') {
+      setErrors((prevState) => ({
+        ...prevState,
+        lastName: 'Last Name is required',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        lastName: '',
+      }));
+    }
+  };
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setErrors((prevState) => ({
+        ...prevState,
+        email: 'Invalid email address',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        email: '',
+      }));
+    }
+  };
+
+  const validatePhone = () => {
+    const phoneRegex = /^[0-9]{10}$/;
+    if (!phoneRegex.test(phone)) {
+      setErrors((prevState) => ({
+        ...prevState,
+        phone: 'Invalid phone number (10 digits)',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        phone: '',
+      }));
+    }
+  };
+
+  const validateDate = () => {
+    if (date === '') {
+      setErrors((prevState) => ({
+        ...prevState,
+        date: 'Date is required',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        date: '',
+      }));
+    }
+  };
+
+  const validateTime = () => {
+    if (time === '') {
+      setErrors((prevState) => ({
+        ...prevState,
+        time: 'Time is required',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        time: '',
+      }));
+    }
+  };
+
+  const validateGuests = () => {
+    if (guests < 1 || guests > 6) {
+      setErrors((prevState) => ({
+        ...prevState,
+        guests: 'Number of guests must be between 1 and 6',
+      }));
+    } else {
+      setErrors((prevState) => ({
+        ...prevState,
+        guests: '',
+      }));
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Handle form submission logic here (you can perform API calls, validations, etc.)
+
+    // Validate form fields before submission
+    validateFirstName();
+    validateLastName();
+    validateEmail();
+    validatePhone();
+    validateDate();
+    validateTime();
+    validateGuests();
+
+    // Check if there are any validation errors
+    if (
+      errors.firstName ||
+      errors.lastName ||
+      errors.email ||
+      errors.phone ||
+      errors.date ||
+      errors.time ||
+      errors.guests
+    ) {
+      return;
+    }
 
     try {
       // Call the submitForm function passed as a prop
@@ -63,94 +194,108 @@ function BookingForm({ dispatch,submitForm}) {
 
   return (
     <div className="booking-container">
-        <form className="booking-form" onSubmit={handleSubmit}>
-          <label htmlFor="first-name">First Name</label>
-          <input
-            type="text"
-            id="first-name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
+      <form className="booking-form" onSubmit={handleSubmit}>
+        <label htmlFor="first-name">First Name</label>
+        <input
+          type="text"
+          id="first-name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          onBlur={validateFirstName}
+          required
+        />
+        <div className="error">{errors.firstName}</div>
 
-          <label htmlFor="last-name">Last Name</label>
-          <input
-            type="text"
-            id="last-name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
+        <label htmlFor="last-name">Last Name</label>
+        <input
+          type="text"
+          id="last-name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          onBlur={validateLastName}
+          required
+        />
+        <div className="error">{errors.lastName}</div>
 
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onBlur={validateEmail}
+          required
+        />
+        <div className="error">{errors.email}</div>
 
-          <label htmlFor="phone">Phone Number</label>
-          <input
-            type="tel"
-            id="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            required
-          />
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          type="tel"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          onBlur={validatePhone}
+          pattern="[0-9]{10}"
+          required
+        />
+        <div className="error">{errors.phone}</div>
 
-          <label htmlFor="res-date">Choose Date</label>
-          <input
-            type="date"
-            id="res-date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            required
-          />
+        <label htmlFor="res-date">Choose Date</label>
+        <input
+          type="date"
+          id="res-date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          onBlur={validateDate}
+          required
+        />
+        <div className="error">{errors.date}</div>
 
-          <label htmlFor="res-time">Choose Time</label>
-          <select
-            id="res-time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            required
-          >
-            {availableTimes.map((availableTime) => (
-              <option key={availableTime} value={availableTime}>
-                {availableTime}
-              </option>
-            ))}
-          </select>
+        <label htmlFor="res-time">Choose Time</label>
+        <select
+          id="res-time"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          onBlur={validateTime}
+          required
+        >
+          {availableTimes.map((availableTime) => (
+            <option key={availableTime} value={availableTime}>
+              {availableTime}
+            </option>
+          ))}
+        </select>
+        <div className="error">{errors.time}</div>
 
-          <label htmlFor="guests">Number of Guests</label>
-          <input
-            type="number"
-            placeholder="1"
-            min="1"
-            max="6"
-            id="guests"
-            value={guests}
-            onChange={(e) => setGuests(parseInt(e.target.value))}
-            required
-          />
+        <label htmlFor="guests">Number of Guests</label>
+        <input
+          type="number"
+          placeholder="1"
+          min="1"
+          max="6"
+          id="guests"
+          value={guests}
+          onChange={(e) => setGuests(parseInt(e.target.value))}
+          onBlur={validateGuests}
+          required
+        />
+        <div className="error">{errors.guests}</div>
 
-          <label htmlFor="occasion">Occasion</label>
-          <select
-            id="occasion"
-            value={occasion}
-            onChange={(e) => setOccasion(e.target.value)}
-            required
-          >
-            <option value="Birthday">Birthday</option>
-            <option value="Anniversary">Anniversary</option>
-            <option value="Engagement">Engagement</option>
-            <option value="Other">Other</option>
-          </select>
+        <label htmlFor="occasion">Occasion</label>
+        <select
+          id="occasion"
+          value={occasion}
+          onChange={(e) => setOccasion(e.target.value)}
+          required
+        >
+          <option value="Birthday">Birthday</option>
+          <option value="Anniversary">Anniversary</option>
+          <option value="Engagement">Engagement</option>
+          <option value="Other">Other</option>
+        </select>
 
-          <input type="submit" value="Make Your Reservation" />
-        </form>
-      )
+        <input type="submit" value="Make Your Reservation" />
+      </form>
     </div>
   );
 }
--- src/components/sections/reservations/BookingForm.test.js
+++ src/components/sections/reservations/BookingForm.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
-import BookingForm from './BookingForm';
-import userEvent from "@testing-library/user-event";
+// import { render, screen } from "@testing-library/react";
+// import BookingForm from './BookingForm';
+// import userEvent from "@testing-library/user-event";
 
 // test('Renders the "First Name" label', () => {
 //     render(<BookingForm />);
@@ -56,25 +56,94 @@ import userEvent from "@testing-library/user-event";
 //     expect(reservationButton).toBeInTheDocument();
 // });
 
-test('initializeTimes function returns expected times', () => {
-    const times = BookingForm.initializeTimes();
-    const expectedTimes = [
-        '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
-    ];
-    expect(times).toEqual(expectedTimes);
+// test('initializeTimes function returns expected times', () => {
+//     const times = BookingForm.initializeTimes();
+//     const expectedTimes = [
+//         '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'
+//     ];
+//     expect(times).toEqual(expectedTimes);
+// });
+
+// test('updateTimes function returns the same value as provided in the state', () => {
+//     const initialState = ['17:00', '18:00', '19:00'];
+//     render(<BookingForm availableTimes={initialState} dispatch={() => {}} />);
+
+//     // Simulate changing the date
+//     const dateInput = screen.getByLabelText('Choose Date');
+//     userEvent.type(dateInput, '2023-09-05');
+
+//     // Get updated available times
+//     const updatedTimes = screen.getByLabelText('Choose Time').textContent.trim().split('\n');
+
+//     // Check if the updated times match the initial state
+//     expect(updatedTimes).toEqual(initialState);
+// });
+
+// BookingForm.test.js
+
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import BookingForm from './BookingForm';
+
+test('HTML5 validation attributes are applied correctly', () => {
+  render(<BookingForm />);
+
+  const firstNameInput = screen.getByLabelText('First Name');
+  const lastNameInput = screen.getByLabelText('Last Name');
+  const emailInput = screen.getByLabelText('Email');
+  const phoneInput = screen.getByLabelText('Phone Number');
+  const dateInput = screen.getByLabelText('Choose Date');
+  const timeInput = screen.getByLabelText('Choose Time');
+  const guestsInput = screen.getByLabelText('Number of Guests');
+
+  expect(firstNameInput).toHaveAttribute('required');
+  expect(lastNameInput).toHaveAttribute('required');
+  expect(emailInput).toHaveAttribute('required');
+  expect(phoneInput).toHaveAttribute('required');
+  expect(phoneInput).toHaveAttribute('pattern', '[0-9]{10}');
+  expect(dateInput).toHaveAttribute('required');
+  expect(timeInput).toHaveAttribute('required');
+  expect(guestsInput).toHaveAttribute('required');
+  expect(guestsInput).toHaveAttribute('min', '1');
+  expect(guestsInput).toHaveAttribute('max', '6');
 });
 
-test('updateTimes function returns the same value as provided in the state', () => {
-    const initialState = ['17:00', '18:00', '19:00'];
-    render(<BookingForm availableTimes={initialState} dispatch={() => {}} />);
+test('Valid and invalid states for JavaScript validation functions', () => {
+    render(<BookingForm />);
 
-    // Simulate changing the date
-    const dateInput = screen.getByLabelText('Choose Date');
-    userEvent.type(dateInput, '2023-09-05');
+    // Simulate invalid input
+    fireEvent.change(screen.getByLabelText('First Name'), { target: { value: '' } });
+    fireEvent.change(screen.getByLabelText('Last Name'), { target: { value: '' } });
+    fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'invalid-email' } });
+    fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '123' } });
+    fireEvent.change(screen.getByLabelText('Choose Date'), { target: { value: '' } });
+    fireEvent.change(screen.getByLabelText('Choose Time'), { target: { value: '' } });
+    fireEvent.change(screen.getByLabelText('Number of Guests'), { target: { value: '7' } });
 
-    // Get updated available times
-    const updatedTimes = screen.getByLabelText('Choose Time').textContent.trim().split('\n');
+    // Check that error messages are displayed for invalid input
+    expect(screen.getByText('First Name is required')).toBeInTheDocument();
+    expect(screen.getByText('Last Name is required')).toBeInTheDocument();
+    expect(screen.getByText('Invalid email address')).toBeInTheDocument();
+    expect(screen.getByText('Invalid phone number (10 digits)')).toBeInTheDocument();
+    expect(screen.getByText('Date is required')).toBeInTheDocument();
+    expect(screen.getByText('Time is required')).toBeInTheDocument();
+    expect(screen.getByText('Number of guests must be between 1 and 6')).toBeInTheDocument();
 
-    // Check if the updated times match the initial state
-    expect(updatedTimes).toEqual(initialState);
-});
+    // Reset the form by providing valid input
+    fireEvent.change(screen.getByLabelText('First Name'), { target: { value: 'John' } });
+    fireEvent.change(screen.getByLabelText('Last Name'), { target: { value: 'Doe' } });
+    fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'john@example.com' } });
+    fireEvent.change(screen.getByLabelText('Phone Number'), { target: { value: '1234567890' } });
+    fireEvent.change(screen.getByLabelText('Choose Date'), { target: { value: '2023-09-20' } });
+    fireEvent.change(screen.getByLabelText('Choose Time'), { target: { value: '18:00' } });
+    fireEvent.change(screen.getByLabelText('Number of Guests'), { target: { value: '4' } });
+
+    // Check that error messages are not displayed for valid input
+    expect(screen.queryByText('First Name is required')).toBeNull();
+    expect(screen.queryByText('Last Name is required')).toBeNull();
+    expect(screen.queryByText('Invalid email address')).toBeNull();
+    expect(screen.queryByText('Invalid phone number (10 digits)')).toBeNull();
+    expect(screen.queryByText('Date is required')).toBeNull();
+    expect(screen.queryByText('Time is required')).toBeNull();
+    expect(screen.queryByText('Number of guests must be between 1 and 6')).toBeNull();
+  });
