e0e0b3c7677ebccf73d321b1e284ef50ec3d5d5a
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-10-02 20:36:20 +0530

    deploying the project

--- src/components/sections/Navigation.js
+++ src/components/sections/Navigation.js
@@ -7,9 +7,9 @@ export default function Navigation(props) {
       {props.device === "mobile" ? (
         ""
       ) : (
-        <Link to="/homepage"><img className="nav-image" src={Navlogo} alt="Little Lemon logo" /></Link>
+        <Link to="/"><img className="nav-image" src={Navlogo} alt="Little Lemon logo" /></Link>
       )}
-      <Link className="hover-effect" to="/homepage"> <h1>Home</h1></Link>
+      <Link className="hover-effect" to="/"> <h1>Home</h1></Link>
       <Link className="hover-effect" to="/about-us"><h1>About</h1></Link>
       <Link className="hover-effect" to="/menu"><h1>Menu</h1></Link>
       <Link className="hover-effect" to="/reservations"><h1>Reservations</h1></Link>
