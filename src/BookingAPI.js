b32a8c433fdacaac3649369501a5b20d05f8e731
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-10-04 19:25:08 +0530

    deploying the project

--- src/BookingAPI.js
+++ src/BookingAPI.js
@@ -1,62 +1,53 @@
 // BookingAPI.js
-// Simulated data store for available reservation times
-const availableTimesData = {
-  '2023-09-01': ['18:00', '19:00', '20:00'],
-  '2023-09-02': ['17:00', '20:00', '21:00'],
-  '2023-09-03': ['18:00', '19:00', '20:00'],
-  '2023-09-04': ['17:00', '20:00', '21:00'],
-  '2023-09-05': ['18:00', '19:00', '20:00'],
-  '2023-09-06': ['17:00', '20:00', '21:00'],
-  '2023-09-07': ['18:00', '19:00', '20:00'],
-  '2023-09-08': ['17:00', '20:00', '21:00'],
-  '2023-09-09': ['18:00', '19:00', '20:00'],
-  '2023-09-10': ['17:00', '20:00', '21:00'],
-  '2023-09-11': ['18:00', '19:00', '20:00'],
-  '2023-09-12': ['17:00', '20:00', '21:00'],
-  '2023-09-13': ['18:00', '19:00', '20:00'],
-  '2023-09-14': ['17:00', '20:00', '21:00'],
-  '2023-09-15': ['18:00', '19:00', '20:00'],
-  '2023-09-16': ['17:00', '20:00', '21:00'],
-  '2023-09-17': ['18:00', '19:00', '20:00'],
-  '2023-09-18': ['17:00', '20:00', '21:00'],
-  '2023-09-19': ['18:00', '19:00', '20:00'],
-  '2023-09-20': ['17:00', '20:00', '21:00'],
-  '2023-09-21': ['18:00', '19:00', '20:00'],
-  '2023-09-22': ['17:00', '20:00', '21:00'],
-  '2023-09-23': ['18:00', '19:00', '20:00'],
-  '2023-09-24': ['17:00', '20:00', '21:00'],
-  '2023-09-25': ['18:00', '19:00', '20:00'],
-  '2023-09-26': ['17:00', '20:00', '21:00'],
-  '2023-09-27': ['18:00', '19:00', '20:00'],
-  '2023-09-28': ['17:00', '20:00', '21:00'],
-  '2023-09-29': ['18:00', '19:00', '20:00'],
-  '2023-09-30': ['17:00', '20:00', '21:00'],
-    // Add more dates and times as needed
-  };
+// Import the required modules
+const { format } = require('date-fns'); // Import date-fns for date formatting
 
-  // Simulated data store for submitted reservations
-  const submittedReservations = [];
+// Function to generate available reservation times based on the date
+const generateAvailableTimes = (startDate, endDate) => {
+  const availableTimesData = {};
+  const oddTimeSlots = ['18:00', '19:00', '20:00'];
+  const evenTimeSlots = ['17:00', '20:00', '21:00'];
+  let currentDate = new Date(startDate);
+  let isOddDate = true;
 
-  // Function to fetch available reservation times for a given date
-  function fetchAPI(date) {
-    return new Promise((resolve, reject) => {
-      // Simulate an API call with a delay (replace with actual API call)
-      setTimeout(() => {
-        const availableTimes = availableTimesData[date] || [];
-        resolve(availableTimes);
-      }, 1000); // Simulate a 1-second delay
-    });
+  while (currentDate <= endDate) {
+    const formattedDate = format(currentDate, 'yyyy-MM-dd'); // Format date as 'yyyy-MM-dd'
+    availableTimesData[formattedDate] = isOddDate ? oddTimeSlots : evenTimeSlots;
+    currentDate.setDate(currentDate.getDate() + 1);
+    isOddDate = !isOddDate;
   }
 
-  // Function to submit a reservation
-  function submitAPI(formData) {
-    return new Promise((resolve, reject) => {
-      // Simulate a successful submission (replace with actual API call)
-      setTimeout(() => {
-        submittedReservations.push(formData);
-        resolve(true);
-      }, 1000); // Simulate a 1-second delay
-    });
-  }
+  return availableTimesData;
+};
 
-  module.exports = { fetchAPI, submitAPI };
+// Define the start and end dates for generating available times
+const startDate = new Date('2023-08-01');
+const endDate = new Date('2033-08-30');
+const availableTimesData = generateAvailableTimes(startDate, endDate);
+
+// Simulated data store for submitted reservations
+const submittedReservations = [];
+
+// Function to fetch available reservation times for a given date
+function fetchAPI(date) {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call with a delay (replace with actual API call)
+    setTimeout(() => {
+      const availableTimes = availableTimesData[date] || [];
+      resolve(availableTimes);
+    }, 1000); // Simulate a 1-second delay
+  });
+}
+
+// Function to submit a reservation
+function submitAPI(formData) {
+  return new Promise((resolve, reject) => {
+    // Simulate a successful submission (replace with actual API call)
+    setTimeout(() => {
+      submittedReservations.push(formData);
+      resolve(true);
+    }, 1000); // Simulate a 1-second delay
+  });
+}
+
+module.exports = { fetchAPI, submitAPI };
